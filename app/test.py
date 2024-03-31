652e2f0594cef1ba176c0232b08ae94353a24827
lunab <luna20617@gmail.com>
2024-03-31 12:38:35 +0000

    test

--- app/test.py
+++ app/test.py
@@ -6,8 +6,15 @@ from espnet2.bin.tts_inference import Text2Speech
 tts = Text2Speech.from_pretrained("mio/tokiwa_midori")
 print('[Info] model loaded')
 
-tts_output = tts("こんにちは、クトリ・ノタ・ セニオリスです。 終末なにしてますか？ 忙しいですか？ 救ってもらっていいですか？")
+text = '''
+こんにちは、クトリ・ノタ・ セニオリスです。 
+終末なにしてますか？ 
+忙しいですか？ 
+救ってもらっていいですか？
+'''.strip()
+
+tts_output = tts(text)
 print(tts_output)
 
-soundfile.write("/contents/tts.wav", tts_output['wav'].numpy(), tts.fs, 'PCM_16')
-playsound.playsound("/contents/tts.wav")
+soundfile.write("contents/tts.wav", tts_output['wav'].numpy(), tts.fs, 'PCM_16')
+# playsound.playsound("contents/tts.wav")
